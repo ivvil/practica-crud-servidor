89712755c6e5a17080c5f30ffa18a92f3f8f0484
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2025-02-07 00:24:05 +0100

    Add template, router and htmx stuff

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs;
+        with phpPackages;
+        with phpExtensions; [
+          mariadb
+          laravel
+          composer
+          mysql84
+          xdebug
+          php
+        ];
+      };
+
+      # apps = rec {
+      #   dev = utils.lib.mkApp {
+      #     drv = pkgs.writeShellScriptBin "composer-dev" ''
+      #         ${pkgs.phpPackages.composer}/bin/composer run dev
+      #     '';
+      #   };
+
+      #   default = dev;
+      # };
+    });
+}
